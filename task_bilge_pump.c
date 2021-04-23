98bfbc2bbadd519d1d687e1a5d094d4e63513e58
Mikołaj Grzybek <mikolaj.grzybek@outlook.com>
2021-04-23 15:11:03 +0200

    Done & Tested

--- task_bilge_pump.c
+++ task_bilge_pump.c
@@ -1,16 +1,22 @@
 #include "task_bilge_pump.h"
 #include "cmsis_os.h"
 
+/*
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 TIM_HandleTypeDef htim2;
+*/
+
+volatile uint16_t bilge_parameters[2] = {0, 0}; // [0] -- pump current; [1] -- water sensor;
 
 volatile SBT_e_bilge_mode g_bilge_mode = PUMP_AUTO; // Pump mode default set to PUMP_AUTO
+
+
 extern osMailQId  mail;
 
 
-int SBT_Bilge_Controll(SBT_s_bilge_fatal_error bilge_fatal_error){
-	if(bilge_fatal_error.water_detected_current){
+int SBT_Bilge_Auto_Controll(SBT_s_bilge_fatal_error* bilge_fatal_error){
+	if(bilge_fatal_error->water_detected_current){
 		return PUMP_AUTO_ON;
 	}
 	else{
@@ -19,49 +25,88 @@ int SBT_Bilge_Controll(SBT_s_bilge_fatal_error bilge_fatal_error){
 }
 
 
-void SBT_Bilge_Detect_Water_Current(uint16_t bilge_current, SBT_s_bilge_fatal_error bilge_fatal_error){
-	if(bilge_current >= CURRENT_WATER_DETECTED){
-		bilge_fatal_error.water_detected_current = 1;
+void SBT_Bilge_State_Check(SBT_s_bilge_fatal_error* bilge_fatal_error){
+		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, SET);
+		osDelay(MEASUREMENT_DELAY);
+
+		// Obtain current
+
+		// Check for water presence
+		SBT_Bilge_Detect_Water_By_Current(bilge_fatal_error);
+		// Check for pump errors
+		SBT_Bilge_Pump_Error_Analysis(bilge_fatal_error);
+
+		/*
+		if(bilge_fatal_error->water_detected_current){
+			water_detected_times++;
+		}
+		else{
+			water_detected_times = 0;
+		}
+		loop_ctr = 0; */
+
+	}
+
+
+
+void SBT_Bilge_Detect_Water_By_Current(SBT_s_bilge_fatal_error* bilge_fatal_error){
+	if(bilge_parameters[Pump_Current] >= CURRENT_WATER_DETECTED){
+		bilge_fatal_error->water_detected_current = 1;
 		SBT_Bilge_System_Failure(BILGE_WATER_DETECTED_BY_CURRENT);
 		return;
 	}
 	else{
-		bilge_fatal_error.water_detected_current = 0;
+		bilge_fatal_error->water_detected_current = 0;
 		return;
 	}
-// FIX ME! What about current read error?
 }
 
-
-int SBT_Bilge_Error_Analysis(SBT_s_bilge_state_input* input_state, uint8_t bilge_pin_state){
-	if(input_state->bilge_current <= CURRENT_ZERO && bilge_pin_state){
-		return BILGE_CURRENT_ZERO;
-	}
-	else if(input_state->bilge_current >= CURRENT_OVER_CURRENT){
-		return BILGE_OVERCURRENT;
-	}
-	else if(!input_state->water_sensor){ // FIX ME! What about water sensor read error?
-		return WATER_SENSOR_READ_ERROR;
+void SBT_Bilge_Detect_Water_By_Sensor(SBT_s_bilge_fatal_error* bilge_fatal_error){
+	if(bilge_parameters[Water_Sensor]){
+		bilge_fatal_error->water_detected_sensor = 1;
+		SBT_Bilge_System_Failure(BILGE_WATER_DETECTED_BY_SENSOR);
+		return;
 	}
 	else{
-		return NONE;
+		bilge_fatal_error->water_detected_sensor = 0;
+		return;
 	}
 }
 
 
-void SBT_Bilge_Define_Error(SBT_e_bilge_event bilge_event_id, SBT_s_bilge_fatal_error bilge_fatal_error){
-
-	if(bilge_event_id == BILGE_CURRENT_ZERO){
-		bilge_fatal_error.bilge_current_zero = 1;
+void SBT_Bilge_Pump_Error_Analysis(SBT_s_bilge_fatal_error* bilge_fatal_error){
+	if(bilge_parameters[Pump_Current] <= CURRENT_ZERO){
+		bilge_fatal_error->bilge_current_zero = 1;
 		SBT_Bilge_System_Failure(BILGE_CURRENT_ZERO);
+		return;
 	}
-	else if(bilge_event_id == BILGE_OVERCURRENT){
-		bilge_fatal_error.bilge_overcurrent = 1;
+	else if(bilge_parameters[Pump_Current] >= CURRENT_OVER_CURRENT){
+		bilge_fatal_error->bilge_overcurrent = 1;
 		SBT_Bilge_System_Failure(BILGE_OVERCURRENT);
+		return;
+	}
+	else if(bilge_parameters[Pump_Current] < 0){ // FIX ME
+		bilge_fatal_error->current_read_error = 1;
+		SBT_Bilge_System_Failure(CURRENT_READ_ERROR);
+		return;
 	}
 	else {
-		bilge_fatal_error.bilge_current_zero = 0;
-		bilge_fatal_error.bilge_overcurrent = 0;
+		bilge_fatal_error->bilge_current_zero = 0;
+		bilge_fatal_error->bilge_overcurrent = 0;
+		return;
+	}
+}
+
+
+void SBT_Water_Sensor_Error_Analysis(SBT_s_bilge_fatal_error* bilge_fatal_error){
+	if(bilge_parameters[Water_Sensor] == 0){ // FIX ME
+		bilge_fatal_error->water_sensor_read_error = 1;
+		SBT_Bilge_System_Failure(WATER_SENSOR_READ_ERROR);
+		return;
+	}
+	else{
+		bilge_fatal_error->water_sensor_read_error = 0;
+		return;
 	}
 }
 
@@ -87,7 +132,22 @@ void SBT_Bilge_System_Failure(SBT_e_bilge_event bilge_event_id){
 		}
 
 		case BILGE_WATER_DETECTED_BY_SENSOR:{
-			msg[0]= BILGE_WATER_DETECTED_BY_CURRENT;
+			msg[0]= BILGE_WATER_DETECTED_BY_SENSOR;
+			break;
+		}
+
+		case BILGE_WATER_DETECTED_BY_CURRENT_FOR_TOO_LONG:{
+			msg[0]= BILGE_WATER_DETECTED_BY_CURRENT_FOR_TOO_LONG;
+			break;
+		}
+
+		case WATER_SENSOR_READ_ERROR:{
+			msg[0]= WATER_SENSOR_READ_ERROR;
+			break;
+		}
+
+		case CURRENT_READ_ERROR:{
+			msg[0]= CURRENT_READ_ERROR;
 			break;
 		}
 
@@ -106,112 +166,88 @@ void SBT_Bilge_System_Failure(SBT_e_bilge_event bilge_event_id){
 
 
 void Start_Task_PumpControl(void const * argument){
-	// Set structs values on init
-	BILGE_STATE_INPUT_INIT(bilge_msg);
-	SBT_s_bilge_state_input *p_msg_received = &bilge_msg;
-	BILGE_FATAL_ALARM_INIT(bilge_fatal_error);
+	BILGE_FATAL_ERROR_INIT(fatal_error);
+	SBT_s_bilge_fatal_error *bilge_fatal_error = &fatal_error;
 
 	// Set variables values on init
-	SBT_e_bilge_event bilge_event = NONE;
 	SBT_e_bilge_auto_action bilge_auto_action = PUMP_AUTO_OFF;
 
-	uint8_t bilge_pin_state = NONE;
+	uint8_t water_detected_times = 0;
 	uint8_t loop_ctr = 0;
 	osEvent evt;
-
-	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1); // WIP raczej nie tu
-
+//delay 3s for boat startup
+//pump seft test for zero current/, over current and short current
 	while(1){
-		// Get data from queue
-		evt = osMailGet(mail, TASK_BILGE_PUMP_COMMUNICATION_DELAY);
-
 		// Connection timeout check
 		if(evt.status == osEventMail){
-			p_msg_received = evt.value.p; // Process values update when available
 			if(bilge_auto_action == PUMP_AUTO_TIMEOUT){
 				g_bilge_mode = PUMP_AUTO; // Turn on Pump Auto Mode when return from timeout
+			}
+
+			if(g_bilge_mode != PUMP_OFF){
+//				 if(loop_ctr >= MEASUREMENT_PERIOD && g_bilge_mode != PUMP_OFF){ // What about measure during error?
+
+				SBT_Bilge_State_Check(bilge_fatal_error);
+				if(water_detected_times >= WATER_DETECTED_CRITICAL_TIME){
+					SBT_Bilge_System_Failure(BILGE_WATER_DETECTED_BY_CURRENT_FOR_TOO_LONG);
+				}
 			}
 		}
 		else{
 			SBT_Bilge_System_Failure(TIMEOUT);
 			bilge_auto_action = PUMP_AUTO_TIMEOUT; // Variable to check when return from timeout
 
-			if(bilge_fatal_error.bilge_current_zero|| bilge_fatal_error.bilge_overcurrent){
+			if(bilge_fatal_error->bilge_current_zero || bilge_fatal_error->bilge_overcurrent){
 				g_bilge_mode = PUMP_OFF; // Cannot turn on pump when there is no info about end of error
 			}
 			else{
 				g_bilge_mode = PUMP_ON;
+
+//				if(loop_ctr >= MEASUREMENT_PERIOD && g_bilge_mode != PUMP_OFF){ // What about measure during error?
+
+				SBT_Bilge_State_Check(bilge_fatal_error);
+				if(water_detected_times >= WATER_DETECTED_CRITICAL_TIME){
+					SBT_Bilge_System_Failure(BILGE_WATER_DETECTED_BY_CURRENT_FOR_TOO_LONG);
+				}
 			}
-		}
 
 
-		// Can we check current again when bilge_current_zero || bilge_overcurrent ???
-
-		// Pump's current analysis. Roughly every 30 seconds
-		if(loop_ctr >= MEASUREMENT_PERIOD && g_bilge_mode != PUMP_OFF){
-			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, SET);
-			osDelay(MEASUREMENT_DELAY);
-			// Obtain current;
-
-			// Check for bilge system error
-			bilge_event = SBT_Bilge_Error_Analysis(p_msg_received, bilge_pin_state);
-			// Handle bilge system error
-			SBT_Bilge_Define_Error(bilge_event, bilge_fatal_error);
-			// Check for water presence
-			SBT_Bilge_Detect_Water_Current(p_msg_received->bilge_current, bilge_fatal_error);
-
-			loop_ctr = 0;
-
-		}
-
-		// Get data from DMA
-		HAL_ADC_Start_DMA(&hadc1, &p_msg_received->water_sensor, 1);
-
-		bilge_event = SBT_Bilge_Error_Analysis(p_msg_received, bilge_pin_state); // again? same function 13 lines above
-
-		if(bilge_event == WATER_SENSOR_READ_ERROR) {
-			bilge_fatal_error.water_sensor_read_error = 1;
-			SBT_Bilge_System_Failure(WATER_SENSOR_READ_ERROR);
-		}
-		else {
-			bilge_fatal_error.water_sensor_read_error = 0;
-		}
+		// Water sensor handling
+		// Check for water presence by sensor
+		SBT_Bilge_Detect_Water_By_Sensor(bilge_fatal_error);
+		// Check for water sensor errors
+		SBT_Water_Sensor_Error_Analysis(bilge_fatal_error);
 
 
-		// Read bilge pump pin state
-		bilge_pin_state = HAL_GPIO_ReadPin(LD2_GPIO_Port, LD2_Pin);
 
 		// Pump Mode handling
 		switch(g_bilge_mode){
 			case PUMP_ON:{
 				HAL_GPIO_WritePin(Cooling_Pump_GPIO_Port, Cooling_Pump_Pin, SET);
-				
 				break;
 			}
 
 			case PUMP_OFF:{
 				HAL_GPIO_WritePin(Cooling_Pump_GPIO_Port, Cooling_Pump_Pin, RESET);
-
-				if(bilge_fatal_error.water_detected_sensor) {
-					SBT_Bilge_System_Failure(BILGE_WATER_DETECTED_BY_SENSOR);
-				}
-
 				break;
 			}
 
 			case PUMP_AUTO:{
 				// Fatal error handling
-				if(bilge_fatal_error.bilge_current_zero){
+				if(bilge_fatal_error->bilge_current_zero){
 					HAL_GPIO_WritePin(Cooling_Pump_GPIO_Port, Cooling_Pump_Pin, RESET);
+
+					// change pump check time
 					break;
 				}
-				else if (bilge_fatal_error.bilge_overcurrent){
+				else if (bilge_fatal_error->bilge_overcurrent){
 					HAL_GPIO_WritePin(Cooling_Pump_GPIO_Port, Cooling_Pump_Pin, RESET);
+
+					// change pump check time
 					break;
 				}
 
-				//
-				bilge_auto_action = SBT_Bilge_Controll(bilge_fatal_error);
+				bilge_auto_action = SBT_Bilge_Auto_Controll(bilge_fatal_error);
 
 				switch(bilge_auto_action){
 					case PUMP_AUTO_ON:{
--- task_bilge_pump.h
+++ task_bilge_pump.h
@@ -2,121 +2,128 @@
 #define INC_TASKPUMPCONTROLL_H_
 
 #include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "sbt_can_msg.h"
 
 
-
-// Struct values init macro
-#define BILGE_STATE_INPUT_INIT(X) SBT_s_bilge_state_input X = {.bilge_current = 0, .water_sensor = 0}
 // Struct pump fatal alarms init
-#define BILGE_FATAL_ALARM_INIT(X) SBT_s_bilge_fatal_error X = {.bilge_current_zero = 0, .bilge_overcurrent = 0, .water_sensor_read_error = 0, .water_detected_current = 0, .water_detected_sensor = 0}
+#define BILGE_FATAL_ERROR_INIT(X) SBT_s_bilge_fatal_error X = {.bilge_current_zero = 0, .bilge_overcurrent = 0, .water_detected_current = 0, .water_detected_sensor = 0}
 
 
-// Bilge pump motor's current in [mA]
+// Bilge pump motor's current in [mA] unit
 #define CURRENT_ZERO 0
-#define CURRENT_NO_WATER 100	
-#define CURRENT_WATER_DETECTED 700	
-#define CURRENT_OVER_CURRENT 1500
-// Water sensor values
-#define WATER_SENSOR_WATER_DETECTED	1000
-// Current measurement settings in [ms]
-#define MEASUREMENT_DELAY 200
-#define MEASUREMENT_PERIOD 30
+#define CURRENT_NO_WATER 500
+#define CURRENT_WATER_DETECTED 700
+#define CURRENT_OVERCURRENT 1000
+// Current measurement time settings
+#define CURRENT_PEAK_DELAY 200 // 200  [ms]
+#define MEASUREMENT_DELAY  5000 //now debug usually: 300000UL 5 [min]
+#define MEASUREMENT_DELAY_CHECK_FATAL 10000 // 10 [min]
+#define MEASUREMENT_DELAY_CHECK_WATER_FIRST 15000 // 10 [s]
+#define MEASUREMENT_DELAY_CHECK_WATER_SECOND 20000 // 15 [s]
+// Current pump's current array id
+#define PUMP_CURRENT 99
+// Current calculate consts
+#define V_REF 3300 // [mV]
+#define ADC_RANGE 4096 // [bits]
 // Task Bilge Pump settings
-#define TASK_BILGE_PUMP_COMMUNICATION_DELAY 1000 // [ms]
+#define TASK_BILGE_PUMP_COMMUNICATION_DELAY 1000 // 1 [s]
 
-// Debug purpose
-#define Cooling_Pump_GPIO_Port LD2_GPIO_Port
-#define Cooling_Pump_Pin LD2_Pin
-
-
-
-typedef struct {
-    int16_t bilge_current; // Not sure of those types
-	int16_t water_sensor;  // Not sure of those types
-} SBT_s_bilge_state_input;
 
 typedef enum {
 	BILGE_WATER_DETECTED_BY_CURRENT,
 	BILGE_WATER_DETECTED_BY_SENSOR,
+	BILGE_WATER_DETECTED_BY_CURRENT_FOR_TOO_LONG,
 	BILGE_CURRENT_ZERO,
 	BILGE_OVERCURRENT,
-	WATER_SENSOR_READ_ERROR,
-	// CURRENT_READ_ERROR
-	NONE,
-	TIMEOUT
+	CURRENT_READ_ERROR,
+	BILGE_AUTO_MODE_OFF,
+	BILGE_TIMEOUT
 } SBT_e_bilge_event;
+
+typedef enum {
+	BILGE_PUMP_ON,
+	BILGE_PUMP_OFF,
+	BILGE_PUMP_AUTO
+} SBT_e_bilge_mode;
 
 typedef struct {
 	uint8_t bilge_current_zero;
 	uint8_t bilge_overcurrent;
-	uint8_t water_sensor_read_error;
-	// uint8_t current_read_error;
+	uint8_t current_read_error;
 	uint8_t water_detected_current;
 	uint8_t water_detected_sensor;
 } SBT_s_bilge_fatal_error;
 
-typedef enum {
-	PUMP_ON,
-	PUMP_OFF,
-	PUMP_AUTO
-} SBT_e_bilge_mode;
-
 typedef enum{
-	PUMP_AUTO_OFF,
-	PUMP_AUTO_ON,
-	PUMP_AUTO_TIMEOUT
+	BILGE_PUMP_AUTO_OFF,
+	BILGE_PUMP_AUTO_ON,
 } SBT_e_bilge_auto_action;
 
 
+// -------------------------------------------- PUMP RELATED --------------------------------------------
 
 /**
- * @brief Turns on pump in auto mode if water detected by current
+ * @brief Turns on/off pump in auto mode if water detected by current
  * 
- * @param bilge_fatal_error.water_detected_current
+ * @param bilge_fatal_error - struct with water_detected_by_current var
  * 
  * @retval ID of SBT_e_pump_auto_action
  * 
  */
-int SBT_Bilge_Controll(SBT_s_bilge_fatal_error bilge_fatal_error);
+int SBT_Bilge_Auto_Controll(SBT_s_bilge_fatal_error* bilge_fatal_error);
 
 
 /**
- * @brief Check for water by current value analysis.
+ * @brief Calculates value of current in [mA] and pass it to other pump rel functions
  * 
- * @param bilge_current value
+ * @param bilge_parameters - obtained by ADC via DMA to unit16_t bilge_parameters[100] 
+ * 
+ * @param bilge_fatal_error - struct with errors indicators
+ * 
+ * @retval None
+ */
+void SBT_Bilge_State_Check(uint16_t bilge_parameters, SBT_s_bilge_fatal_error* bilge_fatal_error);
+
+
+/**
+ * @brief Check for water presence by pump's current value analysis.
+ * 
+ * @param pump_current - obtained by ADC via DMA to unit16_t bilge_parameters[100] 
  *
- * @param bilge_fatal_error water_detected_current is set TRUE
- * if water detected
+ * @param bilge_fatal_error - struct with water_detected_by_current var
  *
  * @return None
  */
-void SBT_Bilge_Detect_Water_Current(uint16_t bilge_current,  SBT_s_bilge_fatal_error bilge_fatal_error);
+void SBT_Bilge_Detect_Water_By_Current(uint16_t pump_current, SBT_s_bilge_fatal_error* bilge_fatal_error);
 
 
 /**
- * @brief Analysis bilge pump state
+ * @brief Analysis bilge pump errors
  * 
- * @param input_state - current and water sensor values
+ * @param pump_current - obtained by ADC via DMA to unit16_t bilge_parameters[100]
  *
- * @param bilge_pin_state
- *
- * @retval ID of detected state
- *
- */
-int SBT_Bilge_Error_Analysis(SBT_s_bilge_state_input* input_state, uint8_t bilge_pin_state);
-
-
-/**
- * @brief If fatal error occurs, save it in SBT_s_bilge_fatal_error
- *
- * @param bilge_event_id
- *
- * @param bilge_fatal_error
+ * @param bilge_fatal_error - struct with errors indicators
  *
  * @retval None
+ *
  */
-void SBT_Bilge_Define_Error(SBT_e_bilge_event bilge_event_id, SBT_s_bilge_fatal_error bilge_fatal_error);
+void SBT_Bilge_Pump_Error_Analysis(uint16_t pump_current, SBT_s_bilge_fatal_error* bilge_fatal_error);
 
+
+// -------------------------------------- WARER SENSOR RELATED ----------------------------------------
+/**
+ * @brief Indicates water presence by water sensor analysis
+ * 
+ * @param bilge_fatal_error - struct with water_detected_by_sensor var
+ * 
+ * @retval None
+ */
+void SBT_Bilge_Water_Sensor(SBT_s_bilge_fatal_error* bilge_fatal_error);
+
+
+// ----------------------------------------- SYSTEM RELATED -------------------------------------------
 
 /**
  * @brief Sents error messeage to CAN queue
@@ -137,6 +144,35 @@ void SBT_Bilge_System_Failure(SBT_e_bilge_event bilge_event_id);
  * @retval None
  *
  */
-void Start_Task_PumpControl(void const * argument);
+void StartTaskBilgePump(void const * argument);
+
+
+// Quick test delays
+///////////////////////////////////////////////////////////////////////////////////////
+// Timer .h  file
+void delayUs(int16_t us);
+
+void delayMs(int32_t ms);
+
+float map(float xin, float xa, float xb, float ya, float yb);
+
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+// #define UNUSED(x) (void)(x)
+
+//timer
+
+// set lastExec to current time
+void timerArm(uint32_t* lastExecTime);
+
+//add delay to last execution time. Use olny after timer expired.
+void timerArmAgain(uint32_t* lastExecTime, uint32_t delay_ms);
+
+// return remaining time to complete delay ( before, positive
+int64_t timerGet(uint32_t lastExecTime, uint32_t delay_ms );
+
+//return remaining time or 0
+uint32_t timerGetRemaining(uint32_t lastExecTime, uint32_t delay_ms );
+///////////////////////////////////////////////////////////////////////////////////////
 
 #endif /* INC_TASKPUMPCONTROLL_H_ */
